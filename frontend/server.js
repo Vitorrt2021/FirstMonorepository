90d1cbf13820b78cb1c6a701a8b31ebaec8297b5
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-01-21 19:19:20 -0300

    Feat: adding process.env.PORT in frontend

--- frontend/server.js
+++ frontend/server.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app =  express()
 
 app.use(express.static('./src'))
-const port = 3005
+
+const port = process.env.PORT || 3005
 app.listen(port,()=>console.log(`Ouvindo na porta ${port} ...`))
