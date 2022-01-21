438a55f7ea8d4cd59ee2e9ef3bc154b37f64f47c
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-01-21 19:50:17 -0300

    Fixed: adding dirname in static files

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app =  express()
 
-app.use(express.static('./src'))
+app.use(express.static(__dirname + '/src'))
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname+'/src/index.html')
+// })
 
 const port = process.env.PORT || 3005
 app.listen(port,()=>console.log(`Ouvindo na porta ${port} ...`))
