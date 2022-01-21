b9ed6244c5eb76860cc1218fd67810f171a2d03c
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-01-21 19:12:41 -0300

    Feat: creating npm start for heroku

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const config = require('./src/config/index')
 
 const app = require('./src/app')
 
-const port = config.app.port
+const port = process.env.PORT || 3004
 app.listen(port,()=>console.log(`Ouvindo na porta ${port} ...`))
